7c63a55a5fcb5ddf88825e216593c4527eeb789f
ethanrestrada <etanr25@gmail.com>
2024-05-26 18:51:26 -0600

    Ethan

---
+++ mat/ECGDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+
+namespace mat
+{
+    public class ECGDrawer
+    {
+        private int[] data;
+        private int dataIndex;
+
+        public ECGDrawer(int[] ecgData)
+        {
+            data = ecgData;
+        }
+
+        public void DrawECG(Graphics g, Rectangle bounds)
+        {
+            int x = bounds.X;
+            int y = bounds.Y + bounds.Height / 2;
+
+            for (int i = 0; i < bounds.Width; i++)
+            {
+                g.DrawLine(Pens.Red, x, y, x + 1, y - data[dataIndex]);
+                dataIndex = (dataIndex + 1) % data.Length;
+                x++;
+            }
+        }
+    }
+
+}
---
+++ mat/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace mat
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private ECGDrawer ecgDrawer;
+        private int[] data; // Datos simulados del ECG
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGraph();
+        }
+
+        private void InitializeGraph()
+        {
+            // Configurar el formulario
+            this.Text = "Electrocardiograma";
+            this.ClientSize = new Size(800, 400);
+
+            // Configurar el temporizador
+            timer = new Timer();
+            timer.Interval = 10; // Intervalo de actualización en milisegundos
+            timer.Tick += Timer_Tick;
+
+            // Simular datos del ECG
+            data = new int[800];
+            Random rnd = new Random();
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = rnd.Next(50) + 100; // Valores entre 100 y 150
+            }
+
+            // Inicializar el ECGDrawer
+            ecgDrawer = new ECGDrawer(data);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Redibujar el gráfico
+            this.Refresh();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Dibujar el ECG
+            ecgDrawer.DrawECG(e.Graphics, this.ClientRectangle);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+    }
+
+}
